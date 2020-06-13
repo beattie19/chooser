3e460e643632562fa6b765662e2f61bc9193d305
Stephen Beattie <beattie19@hotmail.co.uk>
2020-06-14 02:15:28 +1000

    Timeout works and buttons render conditionally

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './App.module.scss';
 import OptionList from './components/OptionList/index'
 import InputForm from './components/InputForm/index';
@@ -13,8 +13,15 @@ const App: React.FC = () => {
   const [options, setOptions] = useState<Options>([]);
   const [showDelete, setShowDelete] = useState<boolean>(true);
 
+  const resetChooser = () => {
+    setOptions([]);
+    setShowDelete(true);
+  }
+
   const addOption = (option: Option) => {
     setOptions([option, ...options]);
+    const input = document.getElementsByTagName('form')[0] as HTMLFormElement;
+    input.reset();
   }
 
   const deleteOption = (option: Option) => {
@@ -27,28 +34,22 @@ const App: React.FC = () => {
 
   const startRandomChoice = () => {    
     setShowDelete(false);
-    pickSingleChoice();
   }
 
-  const pickSingleChoice = () => {
-
-      // const randomToRemove = options[Math.floor(Math.random() * options.length)];
-
-      // deleteOption(randomToRemove);
-
-    while(options.length > 1) {
-    //   // const randomToRemove = options[Math.floor(Math.random() * options.length)];
-    //   // console.log(randomToRemove);
-    //   // deleteOption(options[0]);
-    //   // console.log(options);
-      options.pop();
-    }
-  }
+  useEffect(
+    () => {
+      if (!showDelete && options.length > 1) { 
+        let randomToRemove = options[Math.floor(Math.random() * options.length)];
+        setTimeout(() => { deleteOption(randomToRemove); }, 1000);
+      }
+    },
+    [showDelete, options]
+  );
 
   return (
     <div className="App">
       <div className={styles.inputContainer}>
-        <InputForm addOption={addOption} startRandomChoice={startRandomChoice}/>
+        <InputForm addOption={addOption} showDelete={showDelete} startRandomChoice={startRandomChoice} optionCount={options.length} resetChooser={resetChooser}/>
       </div>
       <OptionList options={options} showDelete={showDelete} deleteOption={deleteOption}/>
     </div>
--- src/components/InputForm/index.tsx
+++ src/components/InputForm/index.tsx
@@ -5,9 +5,12 @@ import styles from './styles.module.scss';
 type InputFormProps = {
     addOption: (option: Option) => void;
     startRandomChoice: () => void;
+    optionCount: number;
+    showDelete: boolean;
+    resetChooser: () => void;
 }
 
-const InputForm: React.FC<InputFormProps> = ({addOption, startRandomChoice}) => {
+const InputForm: React.FC<InputFormProps> = ({addOption, startRandomChoice, optionCount, showDelete, resetChooser}) => {
     const textInputRef = useRef<HTMLInputElement>(null);
 
     const handleSubmit = (event: React.FormEvent) => {
@@ -15,9 +18,17 @@ const InputForm: React.FC<InputFormProps> = ({addOption, startRandomChoice}) => 
         const enteredText = textInputRef.current!.value;
         const option: Option = {
             id: Math.random(),
-            text: enteredText
+            text: enteredText.toUpperCase()
         };
         addOption(option);
+    }
+
+    let button;
+
+    if (!showDelete) {
+        button = <button className={`${styles.startButton}`} onClick={resetChooser}>Reset</button>
+    } else if (showDelete) {
+        button = <button className={`${styles.startButton} ${optionCount < 2 ? styles.hide : ''}`} onClick={startRandomChoice}>Choose</button>
     }
 
     return (
@@ -26,9 +37,9 @@ const InputForm: React.FC<InputFormProps> = ({addOption, startRandomChoice}) => 
             <div className="form-control">
                 <input type="text" className={styles.inputText} ref={textInputRef} placeholder="Enter option" required/>
             </div>
-            <button type="submit"><span>&#43;</span></button>
+            {showDelete && <button type="submit"><span>&#43;</span></button>}
         </form>
-        <button onClick={startRandomChoice}>Start</button>
+        {button}
     </>
     )
 }
